58896acde33fe91c00dfaea04f8ce7d6f494d799
FlorianJ67 <106523472+FlorianJ67@users.noreply.github.com>
2022-12-23 09:52:29 +0100

    Update script.js
    comment added

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ convert.addEventListener("input", function(){
 
 // display the value
 function convFranc(valeur){
+        // the content of the variable converted * 6.56 the change for euro to french franc
         converted.textContent = valeur * 6.56;
+        // if the varialbe converted is null or equal to 0 hide the text
         if (converted.textContent == null || converted.textContent == 0) {
             converted.style.opacity = "0";
         } else {
             converted.style.opacity = "1";
+            // if the value return NaN request to redo the prompt
             if (converted.textContent == "NaN") {
                 converted.textContent = "Vous devez saisir une valeur numérique !";
             } else {
+                // add s for plural
                 if (parseInt(converted.textContent) <= 1) {
                     converted.textContent += " franc"
                 } else {
